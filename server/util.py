1c292149e57fe5ac28c45e6324c3767166688afc
mother <chiemeriedroid@gmail.com>
2023-02-06 20:41:12 +0100

    configured util file to predit with the model

--- server/util.py
+++ server/util.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+import numpy as np
 
 
 _locations = None
@@ -7,8 +8,20 @@ _array_columns = None
 _model = None
 
 
-def get_approx_price(loc, sqft, bedrooms, bath):
-    pass
+def get_approx_price(loc, sft, bedrooms, bath):
+
+    try:
+        location_index = _array_columns.index(loc.lower())
+    except:
+        location_index = -1
+
+    x = np.zeros(len(_array_columns))
+    x[0] = sft
+    x[1] = bath
+    x[2] = bedrooms
+    if location_index >= 0:
+        x[location_index] = 1
+    return round(_model.predict([x])[0], 3)
 
 
 def get_locations():
@@ -19,19 +32,22 @@ def load_artifacts():
     print("loading artifacts information...")
     global _array_columns
     global _locations
+    global _model
 
     with open("./artifacts/array_columns.json", "r") as file:
-        _data_columns = json.load(file)["array_columns"]
-        _locations = _data_columns[3:]
+        _array_columns = json.load(file)["array_columns"]
+        _locations = _array_columns[3:]
 
     with open("./artifacts/bengaluru_house_price_model.pickle", "rb") as file:
          _model = pickle.load(file)
 
-    print("Loadinng information completed")
+    print("Information loading completed")
 
 
 
 
 if __name__ =="__main__":
     load_artifacts()
-    print(get_locations())
+    # print(get_locations())
+
+    print("House price: ", get_approx_price("Indira Nagar", 1000, 2, 3))
